87be705130e33b38d13721068623016074e54c5e
yaowenqiang <yaowenqiang111@163.com>
2019-10-11 02:28:39 +0800

    add datarace demo

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		//i := i
+		go func () {
+			fmt.Println(i)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+
