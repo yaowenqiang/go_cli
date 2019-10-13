163939f219da3591fd1f7330cc23e293c45de126
yaowenqiang <yaowenqiang111@163.com>
2019-10-14 01:14:20 +0800

    add shared library demo and some tests

---
+++ mylib/childlib/childlib_test.go
@@ -0,0 +1,10 @@
+package childlib
+import (
+	"testing"
+)
+
+func Test_MoreBasics(t *testing.T) {
+	if 10 - 5 != 5 {
+		t.Error("Failed to subtract correctly")
+	}
+}
---
+++ world.c
@@ -0,0 +1,6 @@
+#include "main.h"
+int main(void)
+{
+    Hello();
+    return 0;
+}
